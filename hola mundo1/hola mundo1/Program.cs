daeda3bdd83b335f876539da906ed8d99037f47f
Alvina Galvez <70040128@continental.edu.pe>
2023-01-05 13:40:50 -0500

    first commit

---
+++ hola mundo1/hola mundo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hola_mundo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo de alvina!");
+        }
+    }
+}
